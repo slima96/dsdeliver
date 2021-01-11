da7f11940082d28c4e1d49033fba25d565203bee
unknown <sandro.lima0016@gmail.com>
2021-01-10 22:53:44 -0300

    Finish front-web

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -6,8 +6,16 @@ export type Product = {
     imageUri: string;
 }
 
-export type OrderLocationdata = {
+export type OrderLocationData = {
     latitude: number;
     longitude: number;
     address: string;
 }
+
+type ProdutcId = {
+    id: number;
+}
+
+export type OrderPayload = {
+    products: ProdutcId[];
+} & OrderLocationData;
